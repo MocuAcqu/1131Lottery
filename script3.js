01ed24659e9bcc26df4acf78e5ece7f63b8ed120
MocuAcqu <148267519+MocuAcqu@users.noreply.github.com>
2024-12-04 16:40:09 +0800

    Add files via upload

--- script3.js
+++ script3.js
@@ -269,11 +269,11 @@ function getAllWinners() {
     return allWinners;
 }
 
-const winnersList = getAllWinners(); // 獲取所有中獎者
+getAllWinners(); // 獲取所有中獎者
 
 // 這個函數會從 Supabase 獲取中獎者的 email 和 name，並下載 CSV 檔案
 // 修改 fetchWinnerEmails 函數，接收 allWinners 作為參數
-async function fetchWinnerEmails(winnersList) {
+async function fetchWinnerEmails(allWinners) {
 
     try {
         // 查詢中獎者的 email 和 name
