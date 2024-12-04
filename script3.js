7fbc1429e7ac90088f704e5294a4120b8c4ffea9
MocuAcqu <148267519+MocuAcqu@users.noreply.github.com>
2024-12-04 21:13:17 +0800

    Add files via upload

--- script3.js
+++ script3.js
@@ -218,7 +218,7 @@ function displayWinners() {
 
     function getAllWinners() {
         // 使用 flatMap 將所有中獎者的名字展平成一個陣列
-        export const allWinners = winners.flatMap(winner => winner.winners);
+        const allWinners = winners.flatMap(winner => winner.winners);
         console.log("allWinners", allWinners);
         return allWinners;
     }
