7188411fb621acbe8ff87f3845a91259e1dc1e25
MocuAcqu <148267519+MocuAcqu@users.noreply.github.com>
2024-12-12 17:30:28 +0800

    Add files via upload

--- script3.js
+++ script3.js
@@ -260,7 +260,7 @@ function downloadCsv() {
 
 
 
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2.47.5/dist/index.js';
 
 const supabaseUrl = 'https://dqcwygutsfiepcbathqm.supabase.co'; // 從 Supabase 設定檔取得
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxY3d5Z3V0c2ZpZXBjYmF0aHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwNzYxODksImV4cCI6MjA0NjY1MjE4OX0.EcmYcKIUZGXOc3PP0ViL58olwi_tCYmFRPlwVe58IAk'; // 從 Supabase 設定檔取得
