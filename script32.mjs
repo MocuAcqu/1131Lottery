8f315a864a80aedf5c00d1c2a015f3c025ea7e9d
MocuAcqu <148267519+MocuAcqu@users.noreply.github.com>
2024-12-12 18:04:29 +0800

    Add files via upload

--- script32.mjs
+++ script32.mjs
@@ -1,5 +1,5 @@
 //import { supabase } from './supabaseClient.js';
-import { createClient } from '/.https://cdn.skypack.dev/@supabase/supabase-js';
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 
 const supabaseUrl = 'https://dqcwygutsfiepcbathqm.supabase.co'; // 從 Supabase 設定檔取得
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxY3d5Z3V0c2ZpZXBjYmF0aHFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEwNzYxODksImV4cCI6MjA0NjY1MjE4OX0.EcmYcKIUZGXOc3PP0ViL58olwi_tCYmFRPlwVe58IAk'; // 從 Supabase 設定檔取得
